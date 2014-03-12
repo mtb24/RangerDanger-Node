252e939757e81e5c6fd31b06dad80928a3fbc12c
mtb24 <kendowney.com@gmail.com>
2014-03-12 12:55:47 -0700

    add access-control headers

--- app.js
+++ app.js
@@ -1,2 +1,9 @@
 // Start sails and pass it command line arguments
-require('sails').lift(require('optimist').argv);
+var app = require('sails');
+app.use(function(req, res, next){
+  res.header('Access-Control-Allow-Origin', '*');
+  res.header('Access-Control-Allow-Methods', 'GET, OPTIONS');
+  res.header('Access-Control-Allow-Headers', 'Content-Type');
+  return next();
+});
+app.lift(require('optimist').argv);
