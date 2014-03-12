0f69c715be72919dedf2d8fccd277da39a7f60d2
mtb24 <kendowney.com@gmail.com>
2014-03-12 14:30:38 -0700

    add methods to access-control headers

--- app.js
+++ app.js
@@ -2,7 +2,7 @@
 var app = require('sails');
 app.use(function(req, res, next){
   res.header('Access-Control-Allow-Origin', '*');
-  res.header('Access-Control-Allow-Methods', 'GET, OPTIONS');
+  res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
   res.header('Access-Control-Allow-Headers', 'Content-Type');
   return next();
 });
