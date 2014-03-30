ce9edef1e5f144da8b4c70893ec5693c1fde6d4c
mtb24 <kendowney.com@gmail.com>
2014-03-29 18:42:08 -0700

    remove nodetime from project

--- app.js
+++ app.js
@@ -1,12 +1,3 @@
-
-/* nodetime logging */
-if(process.env.NODETIME_ACCOUNT_KEY) {
-  require('nodetime').profile({
-    accountKey: process.env.NODETIME_ACCOUNT_KEY,
-    appName: 'Ranger Danger',
-  });
-}
-
 // Start sails and pass it command line arguments
 var app = require('sails');
 app.use(function(req, res, next){
