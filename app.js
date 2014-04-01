7f23cfcf4d8a1aa63442c030b1bddbfa439f69ca
mtb24 <kendowney.com@gmail.com>
2014-03-31 19:49:41 -0700

    Revert changes

--- app.js
+++ app.js
@@ -1,9 +1,2 @@
 // Start sails and pass it command line arguments
-var app = require('sails');
-app.use(function(req, res, next){
-  res.header('Access-Control-Allow-Origin', '*');
-  res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
-  res.header('Access-Control-Allow-Headers', 'Content-Type');
-  return next();
-});
-app.lift(require('optimist').argv);
+require('sails').lift(require('optimist').argv);
