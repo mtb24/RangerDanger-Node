fb5a2592e7992a68759f9804cdee3dab2d87cf5d
mtb24 <kendowney.com@gmail.com>
2014-06-08 23:32:13 -0700

    change adapter info

--- config/adapters.js
+++ config/adapters.js
@@ -39,12 +39,13 @@ module.exports.adapters = {
   mongo: {
     /* Heroku - MONGOHQ_URL */
     module    : 'sails-mongo',
-    url      : 'mongodb://heroku_app22504694:lhd9ne3lgildpiqut5fauij7p4@ds033679.mongolab.com:33679/heroku_app22504694',
+    url      : 'mongodb://rangerdanger:53OHYlXqvP2g@ds033679.mongolab.com:33679/heroku_app22504694'
+    //url      : 'mongodb://heroku_app22504694:lhd9ne3lgildpiqut5fauij7p4@ds033679.mongolab.com:33679/heroku_app22504694'
     /*
     host      : 'localhost',
     port      : 27017,
-    username  : '',
-    password  : '',
+    username  : 'rangerdanger',
+    password  : '53OHYlXqvP2g',
     database  : 'rangerdanger',
     */
   }
